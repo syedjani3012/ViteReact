7051b790ffbf08477d4503ab5c83906ecaf4ec82
syedjani3012 <syedjani3012@gmail.com>
2024-10-16 18:21:10 +0530

    different function calls in render

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,28 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+//function can also be declared here and can be called inside render
+function Fun(){
+  return(
+    <h1>So funny</h1>
+  )
+}
+
+//creating a tag, it will get converted into tree by react.render
+const test1=(
+  <a href='https://google.com' target='blank'>click here to navigate to google</a>
 )
+
+//creating react element 
+const reactelement=React.createElement(
+  'a',{
+    href:'https://google.com',
+    target: 'blank'
+  }, "using react created element"
+)
+
+createRoot(document.getElementById('root')).render(
+    <App/>
+)
+
